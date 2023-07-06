b039b272b5193ddd24cf4d96e347ef03aa367191
Haeun Suh <suhhaeun87@gmail.com>
2023-07-06 06:46:04 -0500

    Added for in advance

---
+++ BASIC_C/Ch08/Conditional.c
@@ -0,0 +1,14 @@
+/*
+    Title: Conditional statement
+    Description: Understanding the concept of the Conditional statement
+    FileName: Contitional.c
+    Modified: July 6th 2023
+    Writer: Haeun Suh
+*/
+
+#include <stdio.h>
+/*
+    Chapter 08 Branching of Flows Based on Conditionss
+    * Futher detail will be skipped except practice codes.
+    * To be updated
+*/

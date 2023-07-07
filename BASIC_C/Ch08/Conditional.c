7b55931ed153b1788f5f01d8255884a45ae8ed0f
Haeun Suh <suhhaeun87@gmail.com>
2023-07-07 06:34:53 -0500

    Some modification1

--- BASIC_C/Ch08/Conditional.c
+++ BASIC_C/Ch08/Conditional.c
@@ -9,20 +9,17 @@
 #include <stdio.h>
 /*
     Chapter 08 Branching of Flows Based on Conditionss
-    * if~else if~else is equivalent to nested form of if~else
+    * if~else if~else is equivalent to nested form of if~else.
     * goto: A keyword used to move the flow of a program to a desired location.
         => requires a specific label used for positioning.
     * Futher detail will be skipped except practice codes.
-
 */
 
 void ifBasic(void)
 {
     int num;
-
     printf("Enter any integer: ");
     scanf("%d", &num);
-
     if(num < 0) printf("Smaller than 0! \n");
     if(num > 0) printf("Bigger than 0! \n");
     if(num == 0) printf("It is 0!! \n");
@@ -32,21 +29,17 @@ void calOne(void)
 {
     int opt;
     double num1, num2, result;
-
     while (1){
         printf("[Menu] (1) ADD (2) SUB (3) MUL (4) DiV (0) Exit \n");
         printf("[SELECT]: ");
         scanf("%d", &opt);
-
         if (opt < 0 || opt > 4) {
             printf("Please select from Menu!! \n");
             continue;
         }
         else if (opt == 0) break;
-
         printf("Enter two real numbers: ");
         scanf("%lf %lf", &num1, &num2);
-
         switch(opt){
             case 1:
                 result = num1 + num2;
@@ -64,17 +57,13 @@ void calOne(void)
                 result = -1;
                 break;
         }
-
-        printf("Result: %f \n", result);
-        
+        printf("Result: %f \n", result);      
     }
-
 }
 
 void mul3mul4(void)
 {
     int num;
-
     for(num=1; num<100; num++){
         if( num%3==0 || num%4==0 ) // Since '==' has higher priority than '||' parenthesis can be omitted.
         printf("%d is Multiples of 3 or 4! \n", num);
@@ -84,10 +73,8 @@ void mul3mul4(void)
 void ifElseBasic(void)
 {
     int num;
-
     printf("Enter integer: ");
     scanf("%d", &num);
-
     if(num<0) printf("Smaller than 0! \n");
     else printf("Not smaller than 0! \n");
     // In conditional ternary operator form
@@ -97,33 +84,27 @@ void ifElseBasic(void)
 void ternaryOp(void)
 {
     int num, abs;
-
     printf("Enter integer: ");
     scanf("%d", &num);
-
     abs = num > 0 ? num : num*(-1);
     printf("ABS of %d: %d \n", num, abs);
-
 }
 
 void whenOver5000(void)
 {
     int sum=0, num=0;
-
     while(1)
     {
         sum+=num;
         if(sum>5000) break;
         num++;
     }
-
     printf("sum: %d, num: %d \n", sum, num);
 }
 
 void mul2mul3(void)
 {
     int num;
-
     printf("BEGIN ");
     for(num=1; num<20; num++){
         if( num%2==0 || num%3==0 ) continue;
@@ -135,10 +116,8 @@ void mul2mul3(void)
 void englishSchool(void)
 {
     int num;
-
     printf("Enter integer range[1,5]: ");
     scanf("%d", &num);
-
     switch(num){
         case 1:
             printf("1 is ONE \n");
@@ -163,11 +142,9 @@ void englishSchool(void)
 void advancedEnglishSchool(void)
 {
     char sel;
-
     printf("M Morning, A Afternoon, E Evening \n");
     printf("SELECT: ");
     scanf("%c", &sel);
-
     switch(sel){
         case 'M':
         case 'm':
@@ -189,14 +166,11 @@ void advancedEnglishSchool(void)
 void goToBasic(void)
 {
     int num;
-
     printf("Enter positive integer: ");
     scanf("%d", &num);
-
     if(num == 1) goto ONE;
     else if(num==2) goto TWO;
     else goto OTHER;
-
     ONE:
         printf("ENTERED 1 \n");
         goto END;

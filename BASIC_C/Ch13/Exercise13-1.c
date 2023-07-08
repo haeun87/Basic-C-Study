226258d07b72ea591ff612bc6a9a7752a07f7c40
Haeun Suh <suhhaeun87@gmail.com>
2023-07-08 05:08:07 -0500

    Added as marks

---
+++ BASIC_C/Ch13/Exercise13-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+Exercise 13-1
+*/
+
+// Problem 1
+int problem1(void)
+{
+    return 0;
+}
+// Problem 2
+
+// Problem 3
+
+// Problem 4
+
+
+int main(void)
+{
+    problem1();
+    printf("\n");
+   
+    return 0;
+}

cabe22b9aa7daa4c4b3015f934567b74704cf0e9
Haeun Suh <suhhaeun87@gmail.com>
2023-07-08 05:06:14 -0500

    Added as mark

---
+++ BASIC_C/Ch13/PointerArray.c
@@ -0,0 +1,21 @@
+/*
+    Title: Pointer and Array
+    Description: Understand the operation of pointers related to array data types.
+    FileName: PointerArray.c
+    Modified: July 9th 2023
+    Writer: Haeun Suh
+*/
+
+#include <stdio.h>
+/*
+    Chapter 13 Pointer and Array.
+    * To be continded at pp.289
+    * Futher detail will be skipped except practice codes.
+
+*/
+
+
+int main(void)
+{
+    return 0;
+}

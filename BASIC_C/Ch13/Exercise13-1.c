ef9a3cee78e5a5e6c48f22830c0bf2461e529233
Haeun Suh <suhhaeun87@gmail.com>
2023-07-09 02:43:27 -0500

    Updated Execercise 13-1

--- BASIC_C/Ch13/Exercise13-1.c
+++ BASIC_C/Ch13/Exercise13-1.c
@@ -4,21 +4,86 @@ Exercise 13-1
 */
 
 // Problem 1
-int problem1(void)
+int pointer1(void)
 {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int* ptr = &arr[0];
+
+    printf("%p \n", ptr);
+
+    for(int i=0; i<5; i++){
+        printf("%d => ",arr[i]);
+        *(ptr++) += 2;
+        printf("%d \n", arr[i]);
+    }
+    printf("%p \n", ptr); // Chaged
+
     return 0;
 }
+
 // Problem 2
+int pointer2(void)
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    int* ptr = &arr[0];
+
+    printf("%p \n", ptr);
+
+    for(int i=0; i<5; i++){
+        printf("%d => ", arr[i]);
+        *(ptr+i) += 2;
+        printf("%d \n",  arr[i]);
+    }
+
+
+    printf("%p \n", ptr);// Unchanged
+
+    return 0;
+}
 
 // Problem 3
+int pointer3(void)
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    int* ptr = &arr[4];
+    int sum = 0;
+
+    for(int i=0; i<5; i++) sum += *(ptr--);
+    
+    printf("TOTAL: %d \n", sum);
+
+    return 0;
+}
 
 // Problem 4
+int pointer4(void)
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int* sPtr = &arr[0]; int* ePtr = &arr[5];
 
+    for(int i=0; i<3; i++)
+    {
+        int temp = *sPtr;
+        *(sPtr++) = *ePtr;
+        *(ePtr--) = temp;
+    }
+
+    for(int i=0; i<6; i++) printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
 
 int main(void)
 {
-    problem1();
+    pointer1();
     printf("\n");
-   
+    pointer2();
+    printf("\n");
+    pointer3();
+    printf("\n");
+    pointer4();
+    printf("\n");
+
     return 0;
 }

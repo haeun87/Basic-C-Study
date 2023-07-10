05e046de49ffa2103ec077975662d81d1538e6ee
Haeun Suh <suhhaeun87@gmail.com>
2023-07-10 02:47:18 -0500

    Mark for later work

---
+++ BASIC_C/Ch18/Exercise18-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+Exercise 18-1
+*/
+
+// Problem 1
+int problem1(void)
+{
+    return 0;
+}
+
+int main(void)
+{
+    problem1();
+    printf("\n");
+
+    return 0;
+}
---
+++ BASIC_C/Ch18/PointerMultiDimArray.c
@@ -0,0 +1,23 @@
+/*
+    Title: Multi-dimentional Array and Pointer
+    Description: Understand the relationship between a Multi-dimentional Array and a Pointer.
+    FileName: PointerMultiDimArray.c
+    Modified: July 10th 2023
+    Writer: Haeun Suh
+*/
+
+#include <stdio.h>
+/*
+    Chapter 18 Relationship between a Multi-dimentional Array and a Pointer.
+    * To be continued from pp.372
+    * Futher detail will be skipped except practice codes.
+*/
+
+
+int main(void)
+{
+    triplePointer();
+    printf("\n");
+
+    return 0;
+}

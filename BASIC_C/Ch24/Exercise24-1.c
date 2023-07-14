582337fe8cba062a62b468310a273bc5ec36cbc0
Haeun Suh <suhhaeun87@gmail.com>
2023-07-14 05:25:46 -0500

    Marked for later works

---
+++ BASIC_C/Ch24/Exercise24-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/*
+Exercise 24-1
+*/
+
+// Problem
+int problem(void)
+{
+
+    return 0;
+}
+
+int main(void)
+{
+    problem();
+    printf("\n");
+
+    return 0;
+}
---
+++ BASIC_C/Ch24/FileIO.c
@@ -0,0 +1,24 @@
+/*
+    Title: File Input and Output
+    Description: Learning about File IOs.
+    FileName: FileIO.c
+    Modified: July 15th 2023
+    Writer: Haeun Suh
+*/
+
+#include <stdio.h>
+
+/*
+    Chapter 24 File Input and Output.
+    * To be continued from pp.501
+*/
+int exercise(void){
+    return 0;
+}
+int main(void)
+{
+    exercise();
+    printf("\n");
+
+    return 0;
+}

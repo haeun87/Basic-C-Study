f5d93164f6d94b4276707d631ca90fd380571e6b
Haeun Suh <suhhaeun87@gmail.com>
2023-07-20 06:11:12 -0500

    Updated Ch26 Problem Code

--- BASIC_C/Ch26/Exercise26-1.c
+++ BASIC_C/Ch26/Exercise26-1.c
@@ -2,22 +2,51 @@
 /*
 Exercise 26-1
 */
+#define ADD(A,B,C) ((A)+(B)+(C))
+#define MUL(A,B,C) ((A)*(B)*(C))
 
-// problem
+#define PI 3.1415
+#define AREA(X) ((X)*(X)*PI)
+
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+
+// problem 1
 int problem1(void)
 {
+    int num1, num2, num3;
+
+    printf("Enter Three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    printf("ADD(%d, %d, %d) = %d \n", num1, num2, num3, ADD(num1,num2,num3));
+    printf("MUL(%d, %d, %d) = %d \n", num1, num2, num3, MUL(num1,num2,num3));
+
     return 0;
 }
 
-// problem
+// problem 2
 int problem2(void)
 {
+    double rad;
+
+    printf("Enter The radius: ");
+    scanf("%lf", &rad);
+
+    printf("Area of a circle(rad: %g) = %g \n", rad, AREA(rad));
+
     return 0;
 }
 
-// problem
+// problem 3
 int problem3(void)
 {
+    int num1, num2;
+
+    printf("Enter Two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("MAX(%d, %d) = %d \n", num1, num2, MAX(num1,num2));
+
     return 0;
 }
 
